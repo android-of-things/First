cc3bfbe43675cd423f81df3e41303efb3cb00aec
android-of-things <79271233+android-of-things@users.noreply.github.com>
2021-02-18 21:39:45 +0530

    First commit
    it's first commit

---
+++ ComputerVoiceSystem.java
@@ -0,0 +1,18 @@
+public class ComputerVoiceSystem {
+
+    public static void main(String[] args){
+
+        String Kulir = "SRG";
+        String Kaar = "PDN";
+        String Pani = "GMP";
+        String venil = "";
+
+        String input = "SRGSRGGRS";
+        String[] charArray = input.split("");
+
+        for (int i =0; i<input.length();i++){
+
+        }
+
+    }
+}
